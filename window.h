dec07480abbab54d3f294a0f582dfb7841be5ef3
AleXMicke <alex.micke.am@gmail.com>
2021-12-20 22:15:13 +0300

    window of add

--- window.h
+++ window.h
@@ -1,12 +1,14 @@
 #ifndef WINDOW_H
 #define WINDOW_H
 #include <QtWidgets>
+#include <createsatelitewindow.h>
 
 class window : public QWidget
 {
     Q_OBJECT
 
     public : window(int weight, int hight);
+    private : void ButtonAddSatelite();
     private : QPushButton* But_plus;
     private : QPushButton* But_play;
 
