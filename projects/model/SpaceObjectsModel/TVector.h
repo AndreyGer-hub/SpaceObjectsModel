1fb61d154fdd387fa7bd70d1e2a12392bd6024be
AndreyGer-hub <andrey2000040808@gmail.com>
2021-12-20 22:43:37 +0300

    TVector edited

--- projects/model/SpaceObjectsModel/TVector.h
+++ projects/model/SpaceObjectsModel/TVector.h
@@ -7,6 +7,14 @@ class TVector
 public:
     TVector();
     double x;
+    double y;
+    double z;
+    double Vx;
+    double Vy;
+    double Vz;
+    double ax;
+    double ay;
+    double az;
 };
 
 #endif // TVECTOR_H
